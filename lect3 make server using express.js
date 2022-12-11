b660c34b04e2f1e3f81055be713436ad16bc22d1
Akshat <a_khandelwal@ch.iitr.ac.in>
2022-12-11 14:09:33 +0530

    made server using express

---
+++ lect3 make server using express.js
@@ -0,0 +1,32 @@
+const express = require('express') ;
+const app = express() ;
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    console.log('hello boi');
+    res.send('<h1>Hello</h1>');
+});
+
+app.get('/alert', (req, res) => {
+    console.log('hello boi');
+    res.sendFile('./htmlfiles/about.html',{root:__dirname}); // we have to send the absolute path to it 
+    // if we not want to send the absolute path then write relative path and write the root:directory where this code is present means this file
+
+});
+app.get('/bio', (req, res) => {
+    console.log('hello boi');
+    res.sendFile('C:/Users/HP/programming/10 node js/node js tut/htmlfiles/tp_index.html');
+});
+
+// syntax is res,sendfile('relative url' , callback function)
+
+//redirect
+app.get('/about-me', (req, res) => {
+    console.log('hello boi');
+    res.redirect('/alert');
+});
+
+//404 pge
+app.use((req, res)=>{
+    res.status(404).sendFile('./htmlfiles/404pg.html',{root:__dirname}) ;
+})
