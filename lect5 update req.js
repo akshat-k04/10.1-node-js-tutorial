9f3fc9d21658c2101626b9e7353e440574758abf
Akshat <a_khandelwal@ch.iitr.ac.in>
2022-12-11 20:49:31 +0530

    learned how to dopatch req

---
+++ lect5 update req.js
@@ -0,0 +1,32 @@
+// if want to do update req then we nees to give patch request
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.listen(3000);
+
+
+let user = {};
+
+app.get('/users', (req, res) => {
+    res.send(user);
+})
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    user = req.body;
+    res.send({
+        message: "data received successfully",
+        infoOFuserData: req.body
+    });
+})
+
+app.patch('/users',(req,res)=>{
+    let dataToBeUpdated = req.body ;
+    for(key in dataToBeUpdated){
+        user[key]= dataToBeUpdated[key] ;
+    }
+    res.json({
+        message: "data updated successfully",
+        userData : user
+    }) ;
+})
