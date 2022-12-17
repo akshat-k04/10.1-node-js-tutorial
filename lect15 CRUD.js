40c1b779e0686f62cf8690013054e99f6bbff7fe
Akshat <a_khandelwal@ch.iitr.ac.in>
2022-12-17 22:54:46 +0530

    adding find method

---
+++ lect15 CRUD.js
@@ -0,0 +1,12 @@
+/*  ******CRUD*******
+c means create data in database 
+R means read data from database
+U means update data in database 
+D means delete data in database
+*/
+
+/*   ******* queries ********
+as we use usermodl.create(user) query (modelname.create()) for creating the data in data base
+similarly we have various queries for RUD operations
+create(),deleteOne(),find(),findOne(),updateOne() ,findOneAndDelete(), findOneAndUpdate() etc
+*/
